fc28281926d0d5170fc5e7f635caa23c49659996
Sahil <Sahil2003@gmail.com>
2024-03-26 21:43:29 +0530

    Feat : added JWT authentication

--- build/service/user.js
+++ build/service/user.js
@@ -1,14 +1,32 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const db_1 = require("../lib/db");
 const node_crypto_1 = require("node:crypto");
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const JWT_SECRET = '@scereat@123ash2k35h21l351k3h';
 class UserService {
-    static createUser(payload) {
-        const { firstName, lastName, email, password } = payload;
-        const salt = (0, node_crypto_1.randomBytes)(16).toString('hex');
+    static generateHash(salt, password) {
         const hashedPassword = (0, node_crypto_1.createHmac)('sha256', salt)
             .update(password)
             .digest('hex');
+        return hashedPassword;
+    }
+    static createUser(payload) {
+        const { firstName, lastName, email, password } = payload;
+        const salt = (0, node_crypto_1.randomBytes)(16).toString('hex');
+        const hashedPassword = UserService.generateHash(salt, password);
         return db_1.prismaClient.user.create({
             data: {
                 firstName,
@@ -17,6 +35,24 @@ class UserService {
                 salt,
                 password: hashedPassword
             }
+        });
+    }
+    // Login Password matching (Token creation)
+    static getUserByEmail(email) {
+        return db_1.prismaClient.user.findUnique({ where: { email } });
+    }
+    static getUserToken(payload) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { email, password } = payload;
+            const user = yield UserService.getUserByEmail(email);
+            if (!user)
+                throw new Error('user not found');
+            const userSalt = user.salt;
+            const userHashPassword = UserService.generateHash(userSalt, password);
+            if (userHashPassword !== user.password) {
+                throw new Error('Incorrect Password');
+            }
+            const token = jsonwebtoken_1.default.sign({ id: user.id, email: user.email }, JWT_SECRET);
         });
     }
 }
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,5 +1,7 @@
+import { User } from '../graphql/user'
 import { prismaClient } from '../lib/db'
 import { createHmac, randomBytes } from 'node:crypto'
+import JWT from 'jsonwebtoken'
 
 export interface CreateUserPayload {
   firstName: string
@@ -8,13 +10,28 @@ export interface CreateUserPayload {
   password: string
 }
 
+const JWT_SECRET = '@scereat@123ash2k35h21l351k3h'
+
+export interface GetUserTokenPayload {
+  email: string
+  password: string
+}
+
 class UserService {
-  public static createUser (payload: CreateUserPayload) {
-    const { firstName, lastName, email, password } = payload
-    const salt = randomBytes(16).toString('hex')
+  private static generateHash (salt: string, password: string) {
     const hashedPassword = createHmac('sha256', salt)
       .update(password)
       .digest('hex')
+
+    return hashedPassword
+  }
+
+  public static createUser (payload: CreateUserPayload) {
+    const { firstName, lastName, email, password } = payload
+    const salt = randomBytes(16).toString('hex')
+
+    const hashedPassword = UserService.generateHash(salt, password)
+
     return prismaClient.user.create({
       data: {
         firstName,
@@ -24,6 +41,27 @@ class UserService {
         password: hashedPassword
       }
     })
+  }
+  // Login Password matching (Token creation)
+
+  private static getUserByEmail (email: string) {
+    return prismaClient.user.findUnique({ where: { email } })
+  }
+
+  public static async getUserToken (payload: GetUserTokenPayload) {
+    const { email, password } = payload
+    const user = await UserService.getUserByEmail(email)
+    if (!user) throw new Error('user not found')
+
+    const userSalt = user.salt
+    const userHashPassword = UserService.generateHash(userSalt, password)
+
+    if (userHashPassword !== user.password) {
+      throw new Error('Incorrect Password')
+    }
+
+    const token = JWT.sign({ id: user.id, email: user.email }, JWT_SECRET)
+    return token
   }
 }
 
